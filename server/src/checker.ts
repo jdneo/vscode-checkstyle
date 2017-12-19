42d80f965bd65c1df16e528251e93e6028992c18
Sheng Chen <sheche@microsoft.com>
2017-12-19 11:03:09 +0800

    auto resolve version - server part

--- server/src/checker.ts
+++ server/src/checker.ts
@@ -1,9 +1,68 @@
 'use strict';
 
 import * as cp from 'child_process';
+import * as fse from 'fs-extra';
+import * as os from 'os';
+import * as path from 'path';
+import { ConfigurationType, ICheckStyleSettings } from './checkstyleSetting';
+import { InvalidVersionError, VersionNotExistError } from './errors';
 
 export namespace checker {
-    export async function exec(...args: string[]): Promise<string> {
+
+    export const resourcesPath: string = path.join(os.homedir(), '.vscode-checkstyle', 'resources');
+
+    export async function checkstyle(settings: ICheckStyleSettings, sourcePath: string, force?: boolean): Promise<string | undefined> {
+        const jarPath: string = await ensureJarFileParam(settings.version);
+        const configPath: string = await ensureConfigurationFileParam(settings.configurationFile);
+        if (settings.autocheck || force) {
+            const checkstyleParams: string[] = [
+                '-jar',
+                jarPath,
+                '-c',
+                configPath
+            ];
+            if (settings.propertiesPath) {
+                checkstyleParams.push('-p', settings.propertiesPath);
+            }
+            checkstyleParams.push(sourcePath);
+            return await exec(...checkstyleParams);
+        }
+        return undefined;
+    }
+
+    async function ensureJarFileParam(jar: string): Promise<string> {
+        // test version format
+        if (/^\d\.\d{1,2}(?:\.\d)?$/.test(jar)) {
+            const jarPath: string = path.join(resourcesPath, `checkstyle-${jar}-all.jar`);
+            if (await fse.pathExists(jarPath)) {
+                return jarPath;
+            }
+            throw new VersionNotExistError(jar);
+        }
+
+        // test file path format
+        if (await fse.pathExists(jar)) {
+            return jar;
+        }
+
+        throw new InvalidVersionError();
+    }
+
+    async function ensureConfigurationFileParam(config: string): Promise<string> {
+        switch (config.toLowerCase()) {
+            case ConfigurationType.GoogleChecks:
+            case ConfigurationType.SunChecks:
+                return `/${config.toLowerCase()}.xml`;
+            default:
+                if (await fse.pathExists(config)) {
+                    return config;
+                } else {
+                    throw new Error(`The configuration file ${config} does not exist`);
+                }
+        }
+    }
+
+    async function exec(...args: string[]): Promise<string> {
         return await new Promise((resolve: (ret: string) => void, reject: (e: Error) => void): void => {
             const options: cp.SpawnOptions = {
                 shell: true
