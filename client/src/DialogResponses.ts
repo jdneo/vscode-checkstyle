0e6742fdd24c1f26bc9764a8acd31be6f30423d5
Sheng Chen <sheche@microsoft.com>
2018-02-12 15:29:37 +0800

    user can disable showing the invalid version hint (#91)
    * user can disable the invalid version hint
    
    * change the wording

--- client/src/DialogResponses.ts
+++ client/src/DialogResponses.ts
@@ -3,9 +3,8 @@
 import { MessageItem } from 'vscode';
 
 export namespace DialogResponses {
-    export const skipForNow: MessageItem = { title: 'Skip for now' };
     export const yes: MessageItem = { title: 'Yes' };
-    export const no: MessageItem = { title: 'No' };
     export const openDownloadPage: MessageItem = { title: 'Open download page' };
     export const cancel: MessageItem = { title: 'Cancel', isCloseAffordance: true };
+    export const never: MessageItem = { title: 'Never' };
 }
