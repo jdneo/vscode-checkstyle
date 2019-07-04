78f519b405dcdddf4d648df96a5ef5ec52d355cb
Ye Zhihao <vigilans@cn-sawinark-p4.fareast.corp.microsoft.com>
2019-07-04 11:03:33 +0800

    Add extension side error handling

--- src/commands/executeJavaLanguageServerCommand.ts
+++ src/commands/executeJavaLanguageServerCommand.ts
@@ -2,8 +2,10 @@
 // Licensed under the GNU LGPLv3 license.
 
 import { commands } from 'vscode';
+import { checkstyleChannel } from '../checkstyleChannel';
 import { JavaLanguageServerCommands } from '../constants/commands';
 
 export function executeJavaLanguageServerCommand<T>(...args: any[]): Thenable<T | undefined> {
+    checkstyleChannel.appendLine(`[Info] Executing command: ${JSON.stringify({ id: args[0], arguments: args.slice(1) })}`);
     return commands.executeCommand<T>(JavaLanguageServerCommands.EXECUTE_WORKSPACE_COMMAND, ...args);
 }
--- src/utils/errorUtils.ts
+++ src/utils/errorUtils.ts
@@ -1,15 +1,46 @@
 // Copyright (c) jdneo. All rights reserved.
 // Licensed under the GNU LGPLv3 license.
 
+import { window } from 'vscode';
+import { ErrorCodes, ResponseError } from 'vscode-languageserver-protocol';
 import { checkstyleChannel } from '../checkstyleChannel';
 import { checkstyleStatusBar } from '../checkstyleStatusBar';
 
-export async function handleErrors(error: Error): Promise<void> {
-    if (error['data']) {
-        checkstyleChannel.appendLine(JSON.stringify(error['data']));
-    } else {
-        checkstyleChannel.appendLine(error.toString());
-    }
+export enum CheckstyleErrorCodes {
+    UnsupportedQuickfix = -39001,
+}
 
+export async function handleErrors(error: Error): Promise<void> {
+    if ('code' in error && 'data' in error) {
+        handleResponseErrors(error as ResponseError<any>);
+    } else {
+        checkstyleChannel.appendLine(`[Extension Error] ${error}`);
+    }
     checkstyleStatusBar.showError();
+}
+
+async function handleResponseErrors(error: ResponseError<any>): Promise<void> {
+    switch (error.code) {
+        case CheckstyleErrorCodes.UnsupportedQuickfix:
+            checkstyleChannel.appendLine(`[Checkstyle Error] Unsupported quickfix: ${error.message}`);
+            break;
+        case ErrorCodes.InvalidRequest:
+            checkstyleChannel.appendLine(`[Checkstyle Error] ${error.message}`);
+            window.showErrorMessage(`Checkstyle: ${error.message}`);
+            break;
+        case ErrorCodes.MethodNotFound:
+            checkstyleChannel.appendLine(`[Command Error] Invalid command: ${error.message}`);
+            break;
+        case ErrorCodes.InvalidParams:
+            checkstyleChannel.appendLine(`[Command Error] Invalid command arguments: ${JSON.stringify({
+                message: error.message,
+                arguments: JSON.stringify(error.data),
+            }, undefined, 2)}`);
+            break;
+        case ErrorCodes.InternalError:
+        case ErrorCodes.UnknownErrorCode:
+        default:
+            checkstyleChannel.appendLine(`[Server Error] ${error.message}\n${JSON.stringify(error.data, undefined, 2)}`);
+            window.showErrorMessage('Internal server error occured, please open output channel for detail.');
+    }
 }
