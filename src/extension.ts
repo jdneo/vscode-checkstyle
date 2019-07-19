a5ad5656bd3a86f89a67a2172d8cebab457d97e8
Ye Zhihao <vigilans@foxmail.com>
2019-07-19 13:01:09 +0800

    feat: Support live linting (#185)

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,21 @@
 // Copyright (c) jdneo. All rights reserved.
 // Licensed under the GNU LGPLv3 license.
 
-import { ExtensionContext, FileSystemWatcher, languages, TextEditor, Uri, window, workspace } from 'vscode';
+import { ExtensionContext, FileSystemWatcher, languages, Uri, workspace } from 'vscode';
 import { dispose as disposeTelemetryWrapper, initializeFromJsonFile, instrumentOperation, instrumentOperationAsVsCodeCommand } from 'vscode-extension-telemetry-wrapper';
 import { checkstyleChannel } from './checkstyleChannel';
 import { checkstyleDiagnosticCollector } from './checkstyleDiagnosticCollector';
+import { checkstyleDiagnosticManager } from './checkstyleDiagnosticManager';
 import { checkstyleStatusBar } from './checkstyleStatusBar';
 import { checkstyle } from './commands/check';
 import { fixCheckstyleViolation } from './commands/fix';
 import { setCheckstyleConfiguration } from './commands/setCheckstyleConfiguration';
 import { CheckstyleExtensionCommands } from './constants/commands';
 import { quickFixProvider } from './quickFixProvider';
-import { isAutoCheckEnabled } from './utils/settingUtils';
 
 export async function activate(context: ExtensionContext): Promise<void> {
     await initializeFromJsonFile(context.asAbsolutePath('./package.json'));
     await instrumentOperation('activation', doActivate)(context);
-
-    if (isAutoCheckEnabled()) {
-        checkstyle();
-    }
 }
 
 export async function deactivate(): Promise<void> {
@@ -28,25 +24,17 @@ export async function deactivate(): Promise<void> {
 
 async function doActivate(_operationId: string, context: ExtensionContext): Promise<void> {
     const watcher: FileSystemWatcher = workspace.createFileSystemWatcher('**/*.{[jJ][aA][vV][aA]}', true /* ignoreCreateEvents */);
-    watcher.onDidChange((uri: Uri) => {
-        if (isAutoCheckEnabled()) {
-            checkstyle(uri);
-        }
-    }, null, context.subscriptions);
+
+    checkstyleDiagnosticManager.initialize(context);
 
     watcher.onDidDelete((uri: Uri) => {
         checkstyleDiagnosticCollector.delete(uri);
     }, null, context.subscriptions);
 
-    window.onDidChangeActiveTextEditor((editor: TextEditor | undefined) => {
-        if (editor && isAutoCheckEnabled()) {
-            checkstyle(editor.document.uri);
-        }
-    }, null, context.subscriptions);
-
     context.subscriptions.push(
         checkstyleChannel,
         checkstyleStatusBar,
+        checkstyleDiagnosticManager,
         watcher,
         languages.registerCodeActionsProvider({ scheme: 'file', language: 'java' }, quickFixProvider),
         instrumentOperationAsVsCodeCommand(CheckstyleExtensionCommands.OPEN_OUTPUT_CHANNEL, () => checkstyleChannel.show()),
---
+++ src/features/FileSynchronizer.ts
@@ -0,0 +1,146 @@
+// Copyright (c) jdneo. All rights reserved.
+// Licensed under the GNU LGPLv3 license.
+
+import * as crypto from 'crypto';
+import * as fse from 'fs-extra';
+import * as os from 'os';
+import * as path from 'path';
+import * as vscode from 'vscode';
+
+export class SyncedFile {
+
+    private tempUri?: vscode.Uri;
+
+    constructor(
+        private readonly document: vscode.TextDocument,
+        private readonly synchronizer: FileSynchronizer,
+    ) { }
+
+    public get realUri(): vscode.Uri {
+        return this.document.uri;
+    }
+
+    public get syncUri(): vscode.Uri {
+        return this.tempUri || this.realUri;
+    }
+
+    public open(): void {
+        if (this.tempUri) {
+            return; // Already opened
+        }
+        if (this.document.isDirty) {
+            this.tempUri = this.synchronizer.open(this.document);
+            this.synchronizer.change(this.document);
+        } else {
+            // Delay the temp file creation until changing it
+        }
+    }
+
+    public close(): void {
+        this.tempUri = undefined;
+        this.synchronizer.close(this.document);
+    }
+
+    public async onContentChanged(e: vscode.TextDocumentChangeEvent): Promise<void> {
+        if (!this.tempUri) { // Lazy loading temp file
+            this.tempUri = this.synchronizer.open(this.document);
+        }
+        this.synchronizer.change(e.document, e.contentChanges);
+    }
+}
+
+interface ISyncRequests {
+    open: Map<string, string>; // Real file -> temp file
+    change: Map<string, string>; // Temp file -> new content
+    close: Map<string, string>; // Real file -> temp file
+}
+
+// tslint:disable-next-line: max-classes-per-file
+export class FileSynchronizer implements vscode.Disposable {
+
+    private tempStorage: string = this.getTempStorage();
+    private tempFilePool: Map<string, string> = new Map(); // managed filePath -> tempPath
+    private pending: ISyncRequests = { open: new Map(), change: new Map(), close: new Map() };
+    private lastFlush: Promise<void[]>;
+
+    constructor(private context: vscode.ExtensionContext) {}
+
+    public dispose(): void {
+        fse.remove(this.tempStorage);
+    }
+
+    // Ensure a file created in temp folder and managed by synchronizer
+    public open(document: vscode.TextDocument): vscode.Uri {
+        const tempPath: string = this.getTempPath(document);
+        if (!this.tempFilePool.has(document.uri.fsPath)) {
+            this.pending.open.set(document.uri.fsPath, tempPath);
+        } // Skip if already open
+        return vscode.Uri.file(tempPath);
+    }
+
+    // Change content of temp file that already exists
+    public change(document: vscode.TextDocument, _event?: vscode.TextDocumentContentChangeEvent[]): void {
+        const tempPath: string = this.getTempPath(document);
+        // Skip if not already open and will not be open in the pending requests
+        if (this.tempFilePool.has(document.uri.fsPath) || this.pending.open.has(document.uri.fsPath)) {
+            this.pending.change.set(tempPath, document.getText());
+        }
+    }
+
+    // Delete the temp file, release the management of synchronizer
+    public close(document: vscode.TextDocument): void {
+        const tempPath: string = this.getTempPath(document);
+        if (this.tempFilePool.has(document.uri.fsPath)) {
+            this.pending.close.set(document.uri.fsPath, tempPath);
+        } // Skip if already closed
+    }
+
+    // Do the actual IO operartion, sending out all pending requests
+    public async flush(): Promise<void> {
+        // Wait for last flush to complete
+        await this.lastFlush;
+        const pendingPromises: Map<string, Promise<void>> = new Map();
+
+        for (const [filePath, tempPath] of this.pending.open.entries()) {
+            pendingPromises.set(tempPath, (async (): Promise<void> => {
+                if (!await fse.pathExists(tempPath)) {
+                    await fse.createFile(tempPath);
+                }
+            })());
+            this.tempFilePool.set(filePath, tempPath);
+        }
+
+        for (const [tempPath, content] of this.pending.change.entries()) {
+            pendingPromises.set(tempPath, (async (): Promise<void> => {
+                await pendingPromises.get(tempPath); // Wait for potential open request first
+                await fse.writeFile(tempPath, content);
+            })());
+        }
+
+        for (const [filePath, tempPath] of this.pending.close.entries()) {
+            pendingPromises.set(tempPath, fse.remove(tempPath));
+            this.tempFilePool.delete(filePath);
+        }
+
+        this.pending = { open: new Map(), change: new Map(), close: new Map() };
+        this.lastFlush = Promise.all(pendingPromises.values());
+        await this.lastFlush;
+    }
+
+    private getTempPath(document: vscode.TextDocument): string {
+        const tempPath: string | undefined = this.tempFilePool.get(document.uri.fsPath);
+        if (tempPath) {
+            return tempPath;
+        }
+        const tempFile: string = `${crypto.createHash('md5').update(document.uri.fsPath).digest('hex')}.${document.languageId}`;
+        return path.join(this.tempStorage, tempFile);
+    }
+
+    private getTempStorage(): string {
+        const storagePath: string | undefined = this.context.storagePath;
+        if (!storagePath) {
+            return path.join(os.tmpdir(), `vscode_checkstyle_sync_${Math.random().toString(36).slice(2, 10)}`);
+        }
+        return path.join(storagePath, 'sync');
+    }
+}
