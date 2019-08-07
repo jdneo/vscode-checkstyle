8c0065f1da24f138ea06d557b0b73ee126109dc5
Vigilans <vigilans@foxmail.com>
2019-08-07 12:53:57 +0800

    Listen to configuration file's modification

--- src/extension.ts
+++ src/extension.ts
@@ -10,9 +10,10 @@ import { checkstyleStatusBar } from './checkstyleStatusBar';
 import { checkCode } from './commands/check';
 import { fixCheckstyleViolation } from './commands/fix';
 import { setCheckstyleConfiguration, setServerConfiguration } from './commands/setCheckstyleConfiguration';
+import { BuiltinConfiguration } from './constants/BuiltinConfiguration';
 import { CheckstyleExtensionCommands } from './constants/commands';
 import { quickFixProvider } from './quickFixProvider';
-import { isAutoCheckEnabled } from './utils/settingUtils';
+import { getCheckstyleConfigurationPath, isAutoCheckEnabled } from './utils/settingUtils';
 
 export async function activate(context: ExtensionContext): Promise<void> {
     await initializeFromJsonFile(context.asAbsolutePath('./package.json'));
@@ -24,14 +25,36 @@ export async function deactivate(): Promise<void> {
 }
 
 async function doActivate(_operationId: string, context: ExtensionContext): Promise<void> {
-    const watcher: FileSystemWatcher = workspace.createFileSystemWatcher('**/*.{[jJ][aA][vV][aA]}', true /* ignoreCreateEvents */);
-
     checkstyleDiagnosticManager.initialize(context);
-    await setServerConfiguration();
+
+    const codeWatcher: FileSystemWatcher = workspace.createFileSystemWatcher('**/*.{[jJ][aA][vV][aA]}', true /* ignoreCreateEvents */);
+    codeWatcher.onDidDelete((uri: Uri) => {
+        checkstyleDiagnosticCollector.delete(uri);
+    }, null, context.subscriptions);
+
+    let configWatcher: FileSystemWatcher | undefined;
+    async function refreshConfiguraiton(): Promise<void> {
+        await setServerConfiguration();
+        if (configWatcher) {
+            configWatcher.dispose();
+            configWatcher = undefined;
+        }
+        const configUri: Uri = Uri.parse(getCheckstyleConfigurationPath());
+        if (configUri.scheme === 'file' && !Object.values(BuiltinConfiguration).includes(configUri.fsPath)) {
+            configWatcher = workspace.createFileSystemWatcher(configUri.fsPath);
+            configWatcher.onDidCreate((_uri: Uri) => setServerConfiguration());
+            configWatcher.onDidChange((_uri: Uri) => setServerConfiguration());
+            configWatcher.onDidDelete((_uri: Uri) => setServerConfiguration());
+            context.subscriptions.push(configWatcher);
+        }
+    }
+    await refreshConfiguraiton();
 
     workspace.onDidChangeConfiguration((e: ConfigurationChangeEvent) => {
-        if (e.affectsConfiguration('java.checkstyle.configuration') ||
-            e.affectsConfiguration('java.checkstyle.properties')) {
+        if (e.affectsConfiguration('java.checkstyle.configuration')) {
+            refreshConfiguraiton();
+        }
+        if (e.affectsConfiguration('java.checkstyle.properties')) {
             setServerConfiguration();
         }
         if (e.affectsConfiguration('java.checkstyle.autocheck')) {
@@ -43,15 +66,11 @@ async function doActivate(_operationId: string, context: ExtensionContext): Prom
         }
     }, null, context.subscriptions);
 
-    watcher.onDidDelete((uri: Uri) => {
-        checkstyleDiagnosticCollector.delete(uri);
-    }, null, context.subscriptions);
-
     context.subscriptions.push(
         checkstyleChannel,
         checkstyleStatusBar,
         checkstyleDiagnosticManager,
-        watcher,
+        codeWatcher,
         languages.registerCodeActionsProvider({ scheme: 'file', language: 'java' }, quickFixProvider),
         instrumentOperationAsVsCodeCommand(CheckstyleExtensionCommands.OPEN_OUTPUT_CHANNEL, () => checkstyleChannel.show()),
         instrumentOperationAsVsCodeCommand(CheckstyleExtensionCommands.SET_CHECKSTYLE_CONFIGURATION, async (uri?: Uri) => await setCheckstyleConfiguration(uri)),
