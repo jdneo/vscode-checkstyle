82678fed6fbe992d16493619db3a91c3b803adf2
Vigilans <vigilans@foxmail.com>
2019-09-24 02:56:18 +0800

    Module init failed message regex fix

--- src/utils/errorUtils.ts
+++ src/utils/errorUtils.ts
@@ -21,7 +21,7 @@ export async function handleErrors(error: Error): Promise<void> {
 }
 
 async function handleModuleIntialization(message: string): Promise<void> {
-    const module: string = message.match(/cannot initialize module (.+) -/)![1];
+    const module: string = message.match(/cannot initialize module (.+?) -/)![1];
     const choice: string | undefined = await vscode.window.showErrorMessage(
         `Module ${module} initialization failed. It may be caused by wrong configuraiton or incompatible version.`,
         'Select another version', 'Open Configuration',
