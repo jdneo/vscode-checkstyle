1f17739f4bed4b5d4838eadd870f9ce72f4cb21f
Karl von Randow <karl@xk72.com>
2021-01-13 18:55:11 +1300

    Improve quick fix to combine duplicate codes

--- src/quickFixProvider.ts
+++ src/quickFixProvider.ts
@@ -1,27 +1,47 @@
 // Copyright (c) jdneo. All rights reserved.
 // Licensed under the GNU LGPLv3 license.
 
-import { CodeAction, CodeActionContext, CodeActionKind, CodeActionProvider, Range, Selection, TextDocument } from 'vscode';
+import { CodeAction, CodeActionContext, CodeActionKind, CodeActionProvider, Diagnostic, Range, Selection, TextDocument } from 'vscode';
 import { CheckstyleExtensionCommands } from './constants/commands';
 import { isQuickFixAvailable } from './utils/quickFixUtils';
 
 class QuickFixProvider implements CodeActionProvider {
     public provideCodeActions(document: TextDocument, _range: Range | Selection, context: CodeActionContext): CodeAction[] {
+        const diagnosticsByCode: IDiagnosticsByCode = groupIDiagnosticsByCode(context.diagnostics);
         const codeActions: CodeAction[] = [];
-        for (const diagnostic of context.diagnostics) {
-            if (!isQuickFixAvailable(diagnostic.code)) {
+        for (const code of Object.keys(diagnosticsByCode)) {
+            if (!isQuickFixAvailable(code)) {
                 continue;
             }
-            codeActions.push({
-                title: `Fix '${diagnostic.message}'`,
-                diagnostics: [diagnostic],
-                command: {
-                    title: 'Fix the Checkstyle violation',
-                    command: CheckstyleExtensionCommands.FIX_CHECKSTYLE_VIOLATION,
-                    arguments: [document.uri, document.offsetAt(diagnostic.range.start), diagnostic.code],
-                },
-                kind: CodeActionKind.QuickFix,
-            });
+            const diagnostics: Diagnostic[] = diagnosticsByCode[code];
+            if (diagnostics.length === 1) {
+                const diagnostic: Diagnostic = diagnostics[0];
+                codeActions.push({
+                    title: `Fix '${diagnostic.message}'`,
+                    diagnostics,
+                    command: {
+                        title: 'Fix the Checkstyle violation',
+                        command: CheckstyleExtensionCommands.FIX_CHECKSTYLE_VIOLATION,
+                        arguments: [document.uri, document.offsetAt(diagnostic.range.start), diagnostic.code],
+                    },
+                    kind: CodeActionKind.QuickFix,
+                });
+            } else {
+                codeActions.push({
+                    title: `Fix ${diagnostics.length} Checkstyle ${formatCode(code)} violations`,
+                    diagnostics,
+                    command: {
+                        title: 'Fix the Checkstyle violation',
+                        command: CheckstyleExtensionCommands.FIX_ALL_CHECKSTYLE_VIOLATIONS,
+                        arguments: [
+                            document.uri,
+                            diagnostics.map((diagnostic: Diagnostic) => document.offsetAt(diagnostic.range.start)),
+                            diagnostics.map((diagnostic: Diagnostic) => diagnostic.code),
+                        ],
+                    },
+                    kind: CodeActionKind.QuickFix,
+                });
+            }
         }
 
         if (codeActions.length > 1) {
@@ -41,7 +61,7 @@ class QuickFixProvider implements CodeActionProvider {
             }
 
             codeActions.push({
-                title: `Fix ${offsets.length} Checkstyle violations`,
+                title: `Fix all ${offsets.length} Checkstyle violations`,
                 command: {
                     title: 'Fix the Checkstyle violation',
                     command: CheckstyleExtensionCommands.FIX_ALL_CHECKSTYLE_VIOLATIONS,
@@ -54,4 +74,45 @@ class QuickFixProvider implements CodeActionProvider {
     }
 }
 
+interface IDiagnosticsByCode {
+    [code: string]: Diagnostic[];
+}
+
+function groupIDiagnosticsByCode(diagnostics: Diagnostic[]): IDiagnosticsByCode {
+    const result: IDiagnosticsByCode = {};
+    for (const diagnostic of diagnostics) {
+        if (typeof diagnostic.code !== 'string') {
+            continue;
+        }
+        if (!result[diagnostic.code]) {
+            result[diagnostic.code] = [];
+        }
+        result[diagnostic.code].push(diagnostic);
+    }
+    return result;
+}
+
 export const quickFixProvider: QuickFixProvider = new QuickFixProvider();
+
+function formatCode(str: string): string {
+    if (str.endsWith('Check')) {
+        str = str.substring(0, str.length - 'Check'.length);
+    }
+
+    let result: string = '';
+    let previousIsUpperCase: boolean = false;
+    for (const c of str) {
+        if (c >= 'A' && c <= 'Z') {
+            if (!previousIsUpperCase) {
+                if (result.length > 0) {
+                    result += ' ';
+                }
+                previousIsUpperCase = true;
+            }
+        } else {
+            previousIsUpperCase = false;
+        }
+        result += c;
+    }
+    return result;
+}
